d6ba353f666fb3326fb4f0260c445a9f2896f86e
jaz valencia <angelvalencia9811@gmail.com>
2021-02-19 21:20:46 -0700

    modal de prospectos terminado

--- src/components/Tables/SimpleTable.vue
+++ src/components/Tables/SimpleTable.vue
@@ -7,13 +7,26 @@
         <md-table-cell md-label="Segundo Apellido">{{ item.segundoApellido }}</md-table-cell>
         <md-table-cell md-label="Estatus">{{ item.estatusId }}</md-table-cell>
         <md-table-cell md-label="informacion">
-          <md-button class="md-just-icon md-simple md-primary">
+          <md-button @click="check(item)" class="md-just-icon md-simple md-primary">
             <md-icon>info</md-icon>
             <md-tooltip md-direction="top">mas informacion</md-tooltip>
           </md-button>
         </md-table-cell>
       </md-table-row>
     </md-table>
+    <md-dialog :md-active.sync="first">
+        <md-dialog-title>informacion del prospecto</md-dialog-title>
+        <div v-for="prospecto in prospectoMostrar">
+            <p> <strong> El nombre del prospecto es: </strong>  {{prospecto.nombre}} {{prospecto.primerApellido}} {{prospecto.segundoApellido}}</p>
+            <p><strong>La direccion del prospecto es:</strong> Calle: {{prospecto.calle}}, Numero: {{prospecto.numero}}, Colonia: {{prospecto.colonia}}</p>
+            <p>Codigo Postal: {{prospecto.cp}}</p>
+            <p><strong>RFC:</strong>{{prospecto.rfc}}</p>
+             <p><strong>Telefono:</strong>{{prospecto.rfc}}</p>
+        </div>
+        <md-dialog-actions>
+            <md-button class="md-primary" @click="close()">Cerrar</md-button>
+        </md-dialog-actions>
+    </md-dialog>
   </div>
 </template>
 
@@ -30,10 +43,12 @@ export default {
   data() {
     return {
       selected: [],
-      prospectosArray: []
+      prospectosArray: [],
+      first: false,
+      prospectoMostrar: []
     };
   },
-  methods:{
+  methods: {
      async listarProspectos(){
       let url = "http://localhost:5678/api/prospectos/";
       await axios.get(url).then(response => {
@@ -42,10 +57,22 @@ export default {
       }).catch(e => {
         console.log(e);
       })
+    },
+    check(item) {
+      this.prospectoMostrar = [];
+      console.log(item, "item");
+      this.first = true;
+      this.prospectoMostrar.push(item);
+      console.log(this.prospectoMostrar);
+    },
+    close() {
+      this.prospectoMostrar = [];
+      this.first = false;
     }
   },
-  mounted(){
+  mounted() {
     this.listarProspectos();
+    this.prospectoMostrar= [];
   }
 };
 </script>
