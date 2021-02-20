505ed0989600d1d6453f6482256eb61f831f9767
jaz valencia <angelvalencia9811@gmail.com>
2021-02-20 12:49:17 -0700

    rollo esLint y eliminar notifications

--- src/components/Tables/SimpleTable.vue
+++ src/components/Tables/SimpleTable.vue
@@ -3,11 +3,18 @@
     <md-table v-model="prospectosArray" :table-header-color="tableHeaderColor">
       <md-table-row slot="md-table-row" slot-scope="{ item }">
         <md-table-cell md-label="Nombre">{{ item.nombre }}</md-table-cell>
-        <md-table-cell md-label="Primer Apellido">{{ item.primerApellido }}</md-table-cell>
-        <md-table-cell md-label="Segundo Apellido">{{ item.segundoApellido }}</md-table-cell>
+        <md-table-cell md-label="Primer Apellido">
+          {{ item.primerApellido }}
+        </md-table-cell>
+        <md-table-cell md-label="Segundo Apellido">
+          {{ item.segundoApellido }}
+        </md-table-cell>
         <md-table-cell md-label="Estatus">{{ item.estatusId }}</md-table-cell>
         <md-table-cell md-label="informacion">
-          <md-button @click="check(item)" class="md-just-icon md-simple md-primary">
+          <md-button
+            @click="check(item)"
+            class="md-just-icon md-simple md-primary"
+          >
             <md-icon>info</md-icon>
             <md-tooltip md-direction="top">mas informacion</md-tooltip>
           </md-button>
@@ -15,23 +22,23 @@
       </md-table-row>
     </md-table>
     <md-dialog :md-active.sync="first">
-        <md-dialog-title>informacion del prospecto</md-dialog-title>
-        <div v-for="prospecto in prospectoMostrar">
+      <md-dialog-title>informacion del prospecto</md-dialog-title>
+      <!-- <div v-for="prospecto in prospectoMostrar">
             <p> <strong> El nombre del prospecto es: </strong>  {{prospecto.nombre}} {{prospecto.primerApellido}} {{prospecto.segundoApellido}}</p>
             <p><strong>La direccion del prospecto es:</strong> Calle: {{prospecto.calle}}, Numero: {{prospecto.numero}}, Colonia: {{prospecto.colonia}}</p>
             <p>Codigo Postal: {{prospecto.cp}}</p>
             <p><strong>RFC:</strong>{{prospecto.rfc}}</p>
              <p><strong>Telefono:</strong>{{prospecto.rfc}}</p>
-        </div>
-        <md-dialog-actions>
-            <md-button class="md-primary" @click="close()">Cerrar</md-button>
-        </md-dialog-actions>
+      </div> -->
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="close()">Cerrar</md-button>
+      </md-dialog-actions>
     </md-dialog>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "simple-table",
   props: {
@@ -49,14 +56,17 @@ export default {
     };
   },
   methods: {
-     async listarProspectos(){
+    async listarProspectos() {
+      var m = this;
       let url = "http://localhost:5678/api/prospectos/";
-      await axios.get(url).then(response => {
-        var respuesta = response.data;
-        this.prospectosArray = respuesta;
-      }).catch(e => {
-        console.log(e);
-      })
+      await axios.get(url)
+        .then(response => {
+          var respuesta = response.data;
+          m.prospectosArray = respuesta;
+        })
+        .catch(e => {
+          console.log(e);
+        });
     },
     check(item) {
       this.prospectoMostrar = [];
@@ -72,7 +82,7 @@ export default {
   },
   mounted() {
     this.listarProspectos();
-    this.prospectoMostrar= [];
+    this.prospectoMostrar = [];
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App";
-import VueMaterial from "vue-material";
 
 // router setup
 import routes from "./routes/routes";
@@ -9,7 +8,6 @@ import routes from "./routes/routes";
 // Plugins
 import GlobalComponents from "./globalComponents";
 import GlobalDirectives from "./globalDirectives";
-import Notifications from "./components/NotificationPlugin";
 
 // MaterialDashboard plugin
 import MaterialDashboard from "./material-dashboard";
@@ -28,7 +26,6 @@ Vue.use(VueRouter);
 Vue.use(MaterialDashboard);
 Vue.use(GlobalComponents);
 Vue.use(GlobalDirectives);
-Vue.use(Notifications);
 
 /* eslint-disable no-new */
 new Vue({
